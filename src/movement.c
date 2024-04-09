d66e5f6194b530f71b181f602340775472b43577
codingfrog27 <codingmerlin@gmail.com>
2024-04-09 21:04:17 +0200

    no more walking through walls

--- src/movement.c
+++ src/movement.c
@@ -15,54 +15,59 @@
 
 void	rotate_vector(t_vector *vector, double angle);
 t_vector	rotate_vector_return(t_vector vector, double angle);
-void	move(t_player *player, t_vector dir, bool	subtract);
+void	move(t_player *player, t_vector dir, bool	subtract, t_vector *newpos);
 void	change_dir(t_player	*player, bool looking_right);
 
 void	cub3d_loop(void	*param)
 {
-	t_cub3d *m;
+	t_cub3d		*m;
+	t_vector	newpos;
 
 	m = param;
+	newpos.x = m->player.pos.x;
+	newpos.y = m->player.pos.y;
 
 	if (mlx_is_key_down(m->mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(m->mlx);
 	if (mlx_is_key_down(m->mlx, MLX_KEY_W))
-		move(&m->player, m->player.dir, false);
+		move(&m->player, m->player.dir, false, &newpos);
 	if (mlx_is_key_down(m->mlx, MLX_KEY_S))
-		move(&m->player, m->player.dir, true);
+		move(&m->player, m->player.dir, true, &newpos);
 	if (mlx_is_key_down(m->mlx, MLX_KEY_D))
-		move(&m->player, rotate_vector_return(m->player.dir, M_PI_2), true);
+		move(&m->player, rotate_vector_return(m->player.dir, M_PI_2), true, &newpos);
 	if (mlx_is_key_down(m->mlx, MLX_KEY_A))
-		move(&m->player, rotate_vector_return(m->player.dir, M_PI_2), false);
+		move(&m->player, rotate_vector_return(m->player.dir, M_PI_2), false, &newpos);
 	if (mlx_is_key_down(m->mlx, MLX_KEY_LEFT))
 		change_dir(&m->player, false);
 	if (mlx_is_key_down(m->mlx, MLX_KEY_RIGHT))
 		change_dir(&m->player, true);
+
+	if (m->map->content[(int)newpos.y][(int)newpos.x] == '0')
+	{
+		m->player.pos.x = newpos.x;
+		m->player.pos.y = newpos.y;
+	}
 	ft_bzero(m->wall->pixels, WIDTH * HEIGHT * 4);
 	raycaster(m->raycast);
 }
 
-void	move(t_player *player, t_vector dir, bool	subtract)
+void	move(t_player *player, t_vector dir, bool	subtract, t_vector *newpos)
 {
 	double		speed;
-	t_vector	newpos;
 
 	speed = 2.5 * player->data->mlx->delta_time; //move to struct if changeable or to move_loop if same for move and rotate
 	if (mlx_is_key_down(player->data->mlx, MLX_KEY_LEFT_SHIFT))
 		speed *= 2.5;
 	if(subtract)
 	{
-		newpos.x = player->pos.x - (dir.x * speed);
-		newpos.y = player->pos.y - (dir.y * speed);
+		newpos->x -= (dir.x * speed);
+		newpos->y -= (dir.y * speed);
 	}
 	else
 	{
-		newpos.x = player->pos.x + (dir.x * speed);
-		newpos.y = player->pos.y + (dir.y * speed);
+		newpos->x += (dir.x * speed);
+		newpos->y += (dir.y * speed);
 	}
-	if (player->data->map->content[(int)newpos.y][(int)newpos.x] == '0')
-	player->pos.x = newpos.x;
-	player->pos.y = newpos.y;
 }
 
 
